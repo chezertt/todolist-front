d95550c47a47869e096d0123f6fc81b9a4d61c52
podchez <maxim.payassistant@gmail.com>
2023-09-13 08:45:54 +0300

    uptdate urls for backend API

--- src/components/API/TodoService.jsx
+++ src/components/API/TodoService.jsx
@@ -3,7 +3,8 @@ import axios from 'axios';
 export default class TodoService {
     static async getAll() {
         try {
-            const response = await axios.get('http://localhost:8080/todos')
+            // const response = await axios.get('http://localhost:8080/todos')
+            const response = await axios.get('http://91.201.114.41:8080/todos')
             console.log("Got all todos:\n", response.data)
             return response.data
         } catch (e) {
@@ -15,7 +16,8 @@ export default class TodoService {
     static async saveNewTodo(todo) {
         try {
             console.log("Trying to save new todo:\n", todo)
-            await axios.post('http://localhost:8080/todos', todo)
+            // await axios.post('http://localhost:8080/todos', todo)
+            await axios.post('http://91.201.114.41:8080/todos', todo)
         } catch (e) {
             console.log(e)
         }
@@ -24,7 +26,8 @@ export default class TodoService {
     static async deleteTodoById(id) {
         try {
             console.log("Trying to delete todo by id = ", id)
-            await axios.delete('http://localhost:8080/todos/' + id)
+            // await axios.delete('http://localhost:8080/todos/' + id)
+            await axios.delete('http://91.201.114.41:8080/todos/' + id)
         } catch (e) {
             console.log(e)
         }
